64444018664d9337eb1cd26239e67f05aaeb09e9
MichalZawadzki <zawadzkimichal7@gmail.com>
2016-03-25 15:53:26 +0100

    user profile na sztywno

--- controllers/myProfileController.js
+++ controllers/myProfileController.js
@@ -1,5 +1,13 @@
-var myProfileController = angular.module('myProfileController', []);
-myProfileController.controller('MyProfileController', ['$scope',
-    function ($scope) {
+var myProfileController = angular.module('myProfileController', ['ngResource']);
+myProfileController.controller('MyProfileController', ['$scope', 'User',
+    function ($scope, User) {
         $scope.something = 'MyProfileController';
+        User.getUser(function (data) {
+            $scope.User = data;
+        });
     }]);
+
+myProfileController.factory('User', ['$resource', function ($resource){
+    return $resource('./fixedObject/user-profile.json', {},
+            {getUser: {method:'GET', isArray: false}})
+}]);
