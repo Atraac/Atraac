eefa67491457020a213e6c5ed20ea905dfac8df2
Atraac <macieklewinski@gmail.com>
2016-03-27 15:24:13 +0200

    Zmiana nazwy raiting na rating, podstawowy szablon dodawania transportu

--- controllers/addTransportController.js
+++ controllers/addTransportController.js
@@ -2,4 +2,24 @@ var addTransportController = angular.module('addTransportController', []);
 addTransportController.controller('AddTransportController', ['$scope',
     function ($scope) {
         $scope.something = 'AddTransportController';
+
+        $('.ui.dropdown').dropdown({
+            fields: { name: "description", value: "data-value" },
+            apiSettings: {
+                mockResponse: {
+                    success: true,
+                    results: [
+                        {"description":"Opole","data-value":1},
+                        {"description":"Wrocław","data-value":2},
+                        {"description":"Warszawa","data-value":3},
+                        {"description":"Budapest","data-value":4},
+                        {"description":"Köln","data-value":5}
+                    ]
+                }
+            }
+        });
+
+        $scope.onSubmit = function ()   {
+           
+        }
     }]);
