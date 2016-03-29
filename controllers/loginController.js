fde954ba39c83459864a87f7788d66166d339d62
MichalZawadzki <zawadzkimichal7@gmail.com>
2016-03-29 23:57:57 +0200

    walidacja loginu

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,23 @@
 var loginController = angular.module('loginController', []);
 
-loginController.controller('LoginController', ['$scope',
-    function ($scope) {
+loginController.controller('LoginController', ['$scope', '$location',
+    function ($scope, $location) {
         $scope.something = 'LoginController';
-        $scope.user = {email : '', password: ''};
-        $scope.onLogin = function () {
-            alert("Login: " + $scope.user.email + "\n" + "Hasło: " + $scope.user.password);
+        $scope.user = {};
+        $scope.submitLogin = function () {
+            if ($scope.loginForm.$valid) {
+                $scope.wrongUser = false;
+
+                if($scope.loginForm.email.$modelValue != "s@s" || $scope.loginForm.password.$modelValue != "s") {
+                    $scope.wrongUser = true;
+                    $scope.loginForm.email.$touched = false;
+                    $scope.loginForm.password.$touched = false;
+                    $scope.user = {};
+                }
+                else {
+                    $scope.wrongUser = false;
+                    $location.path("#/");
+                }
+            }
         }
     }]);
