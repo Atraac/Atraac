27fff084c121fa65cd39d4ff1cac67e1c4c72540
Atraac <macieklewinski@gmail.com>
2016-03-29 15:40:07 +0200

    Weryfikacja formularza dodawania transportu, wyslanie POST formularza

--- controllers/addTransportController.js
+++ controllers/addTransportController.js
@@ -1,6 +1,6 @@
 var addTransportController = angular.module('addTransportController', []);
-addTransportController.controller('AddTransportController', ['$scope',
-    function ($scope) {
+addTransportController.controller('AddTransportController', ['$scope', '$location',
+    function ($scope, $location) {
         $('.ui.dropdown').dropdown({
             fields: { name: "name", value: "id" },
             apiSettings: {
@@ -8,15 +8,77 @@ addTransportController.controller('AddTransportController', ['$scope',
             }
         });
 
-        $('#transport-pack-types').dropdown({
+        $('#transport-packtypes').dropdown({
             fields: { name: "type", value: "id" },
             apiSettings: {
                 url: './fixedObject/pack-types.json/{query}'
             }
         });
-        $scope.onSubmit = function ()   {
-            //alert("asd");
-            var returnedText = $('#transport-through').dropdown('get value', returnedText);
-            alert(returnedText);
-        }
+
+        $('.ui.form')
+            .form({
+                fields: {
+                    title: {
+                        identifier: 'transport-title',
+                        rules: [
+                            {
+                                type   : 'empty',
+                                prompt : 'Proszę wprowadzić tytuł ogłoszenia'
+                            }
+                        ]
+                    },
+                    start: {
+                        identifier: 'transport-start',
+                        rules: [
+                            {
+                                type   : 'empty',
+                                prompt : 'Proszę wybrać lokację początkową'
+                            }
+                        ]
+                    },
+                    end: {
+                        identifier: 'transport-end',
+                        rules: [
+                            {
+                                type   : 'empty',
+                                prompt : 'Proszę wybrać lokację końcową'
+                            }
+                        ]
+                    },
+                    through: {
+                        identifier: 'transport-through'
+                    },
+                    packtypes: {
+                        identifier: 'transport-packtypes',
+                        rules: [
+                            {
+                                type   : 'empty',
+                                prompt : 'Proszę wybrać rodzaje transportowanych paczek'
+                            }
+                        ]
+                    },
+                    description: {
+                        identifier: 'transport-description',
+                        rules: [
+                            {
+                                type   : 'empty',
+                                prompt : 'Proszę wprowadzić opis ogłoszenia'
+                            }
+                        ]
+                    }
+                }
+            });
+        $('form').api({
+            url: './fixedObject/addedTransports.json',
+            action: 'addtransport',
+            method: 'post',
+            serializeForm: true,
+            dataType: 'text',
+            onSuccess: function() {
+                // to do - switch view to transportid
+            },
+            onError: function() {
+                alert('Wystąpił błąd połączenia z serwerem');
+            }
+        });
     }]);
