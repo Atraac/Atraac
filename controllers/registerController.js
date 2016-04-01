57d1c263de15238b3e244b648582c301cdd6001d
MichalZawadzki <zawadzkimichal7@gmail.com>
2016-04-01 13:13:47 +0200

    mała poprawka walidacji rejestracji

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,9 +6,11 @@ registerController.controller('RegisterController', ['$scope', 'User',
         $scope.something = 'RegisterController';
         $scope.user = {};
         $scope.correctRegister = false;
+        $scope.duplicateEmail = false;
         $scope.onRegister = function () {
             console.log($scope.user);
             $scope.correctRegister = true;
+            
         }
     }
 ]);
