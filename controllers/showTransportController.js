3bcaf73c3b549d013cc20e1497ac4fc53842f068
MichalZawadzki <zawadzkimichal7@gmail.com>
2016-04-03 23:42:44 +0200

    front pojedynczego transportu, tylko wyswitlenie danych, reszta nie dziala

--- controllers/showTransportController.js
+++ controllers/showTransportController.js
@@ -1,6 +1,14 @@
 var showTransportController = angular.module('showTransportController', []);
 
-showTransportController.controller('ShowTransportController', ['$scope',
-    function ($scope) {
+showTransportController.controller('ShowTransportController', ['$scope', '$http',
+    function ($scope, $http) {
         $scope.something = 'ShowTransportController';
+        $scope.transport = {}
+        $http.get('./fixedObject/showTransport.json')
+            .then(function (response)
+            {
+                $scope.transport = response.data;
+            }, function (error) {
+                $scope.error1 = JSON.stringify(error);
+            });
     }]);
